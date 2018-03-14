5894d6ce9110eda94059e4d81d8ca74b21f52dd8
ffahri <ffahri@users.noreply.github.com>
2018-03-14 11:33:50 +0300

    addec comment created metric domain

---
+++ src/main/java/com/webischia/analysisbackend/Domains/Metric.java
@@ -0,0 +1,19 @@
+package com.webischia.analysisbackend.Domains;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+@Data
+public class Metric {
+    private Date timestamp;
+    private String instance;
+    private Double value;
+
+    //user -> izni var mı o metriğe - > metriği aç
+    //admin -> izni var mı var -> aç
+    //query/geliyor -> spring gidip prometheustan istiyor -> tekrar json olarak androide atıyor
+
+}
