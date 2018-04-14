80787ea0de7fe59c5a3239e2bf0dff19cee395af
ffahri <ffahri@users.noreply.github.com>
2018-04-14 14:06:18 +0300

    downgraded tomcat and activated full auth

--- src/main/java/com/webischia/analysisbackend/Controllers/MetricController.java
+++ src/main/java/com/webischia/analysisbackend/Controllers/MetricController.java
@@ -4,13 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,16 +19,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping({"/api/v1/metric/", "/api/v1/metric"})
-public class MetricController {
 
-    private RestTemplate restTemplate;
+public class MetricController {
 
     @PreAuthorize("hasAuthority('User')")
     @GetMapping("/{rq}")
     public ResponseEntity<String> getById(@PathVariable String rq) throws IOException {
-        String url = "http://192.168.122.160:9090/api/v1/query?query=" + rq;
+
+        String url = "http://52.15.130.207:9090/api/v1/query?query=" + rq;
+
         System.out.println(url);
         UriComponentsBuilder uriBuilder = UriComponentsBuilder
                 .fromUriString(url);
@@ -49,5 +49,16 @@ public class MetricController {
         System.out.println(sb.toString());
         return new ResponseEntity<String>(sb.toString(), HttpStatus.OK); //proxying prometheus todo check security XSS
         //return new ResponseEntity<String>("TEST " + rq, HttpStatus.OK);
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such Order")  // 404
+    public class OrderNotFoundException extends RuntimeException {
+        // ...
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    public ResponseEntity<String> unAuthorised(HttpServletRequest request) throws Exception {
+        return new ResponseEntity<String>("Hello", HttpStatus.OK); //proxying prometheus todo check security XSS
     }
 }
